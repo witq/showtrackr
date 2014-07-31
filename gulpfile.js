9134918e530e5fcbe81d0b7f1b14c8fb69d8e46e
Witek Galecki <w.galecki@clearcode.cc>
2014-07-31 11:24:47 +0200

    This gulp configuration is bougs

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@
   var angularFilesort = require('gulp-angular-filesort'),
       bowerFiles      = require('main-bower-files'),
       cssmin          = require('gulp-minify-css'),
+      es              = require('event-stream'),
       gulp            = require('gulp'),
       gulpif          = require('gulp-if'),
       inject          = require('gulp-inject'),
@@ -19,45 +20,37 @@
 
   gulp.task('help', taskListing);
   gulp.task('compile', ['compile-usemin']);
-  gulp.task('build', ['inject', 'copy']);
+  gulp.task('build', ['inject', 'copy-all', 'less-build']);
   gulp.task('clean', ['clean-public']);
-  gulp.task('copy', ['copy-app', 'copy-bower']);
+  gulp.task('copy', ['copy-app-all', 'copy-bower']);
   gulp.task('inject', ['inject-bower', 'inject-js', 'inject-css']);
-
-  // Watchers
-
-  gulp.task('watch', ['build'], function() {
-
-    gulp.watch(['./src/**/*', '!./src/vendor/**/*'], ['build']);
-
-  });
-
 
   // Subtasks
 
-  gulp.task('less', function() {
+  gulp.task('less-build', function() {
     return gulp
       .src('./src/styles/**/*.less', {base: './src'})
       .pipe(less())
-      .pipe(gulp.dest('./src'));
-  });
-
-  gulp.task('copy-app', ['clean-public'], function() {
-    return gulp
-      .src(['./src/**/*.js', './src/**/*.html', './src/styles/**/*.css', '!./src/vendor/**/*'], {base: './src', read: true})
       .pipe(gulp.dest('./public'));
   });
 
-  gulp.task('copy-bower', ['clean-public'], function() {
+  gulp.task('copy-app', function() {
     return gulp
-      .src(bowerFiles(), {base: './src', read: true})
+      .src(['./src/**/*.js'], {base: './src', read: true})
       .pipe(gulp.dest('./public'));
   });
 
-  gulp.task('inject-bower', ['clean-public'], function() {
+  gulp.task('copy-all', function() {
+    var app = gulp.src(['./src/**/*.js', './src/**/*.html'], {base: './src', read: true}),
+        vendor = gulp.src(bowerFiles(), {base: './', read: true});
+    return es.merge(app, vendor)
+      .pipe(gulp.dest('./public'));
+  });
+
+  gulp.task('inject-bower', ['inject-css'], function() {
     return gulp
       .src('./src/index.html')
-      .pipe(inject(gulp.src(bowerFiles(), {read: false}), {
+      .pipe(inject(gulp.src(bowerFiles(), {base: './', read: true}), {
         name: 'bower',
         endtag: '<!-- endbower -->',
         relative: true
@@ -65,7 +58,7 @@
       .pipe(gulp.dest('./src'));
   });
 
-  gulp.task('inject-js', ['clean-public'], function() {
+  gulp.task('inject-js', function() {
     return gulp
       .src('./src/index.html')
       .pipe(inject(gulp.src('./src/app/**/*.js').pipe(angularFilesort()), {
@@ -76,7 +69,7 @@
       .pipe(gulp.dest('./src'));
   });
 
-  gulp.task('inject-css', ['clean-public', 'less'], function() {
+  gulp.task('inject-css', ['inject-js'], function() {
     return gulp
       .src('./src/index.html')
       .pipe(inject(gulp.src('./src/styles/*.css', {read: true}).pipe(less()), {
@@ -95,7 +88,7 @@
 
   gulp.task('compile-usemin', ['inject', 'clean-public'], function() {
     return gulp
-      .src('./src/index.html')
+      .src('./public/index.html')
       .pipe(usemin({
         js: [ngAnnotate(), uglify()],
         css: [cssmin()]
